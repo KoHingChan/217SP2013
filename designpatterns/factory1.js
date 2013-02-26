8087e62d9c4c0176f8dc9fb56f35c225054e17ce
Kohing Chan unknown student <kc99@afsconnect1.njit.edu>
2013-02-25 21:09:25 -0500

    modified:   designpatterns/designpatterns.html 	modified:   designpatterns/designpatterns2.html 	modified:   designpatterns/designpatterns3.html 	modified:   designpatterns/factory1.js

---
+++ designpatterns/factory1.js
@@ -0,0 +1,42 @@
+function sandwich (stuff) {
+    this.meat = stuff.meat || "ham";
+    this.cheese = stuff.cheese || "american";
+    this.size = stuff.size || "medium";
+    this.bread = stuff.bread || "white";
+    
+}
+
+function drink (stuff) {
+    this.soda = stuff.soda || "coke";
+    this.juice = stuff.juice || "apple";
+    this.water = stuff.water || "water";
+    this.coffee = stuff.coffee || "black";
+    
+}
+
+function lunchfactory() {}
+
+lunchfactory.prototype.lunchClass = sandwich;
+
+lunchfactory.prototype.createMeal = function(stuff) {
+    if (stuff.lunchClass === "sandwich"){
+        this.lunchClass = sandwich;
+    }else{
+        this.lunchClass = drink;
+    }
+    return new this.lunchClass (stuff);
+};
+
+var firstitem = new lunchfactory();
+var firstmeal = firstitem.createMeal({
+    lunchClass: "sandwich",
+    meat:  "roast beef",
+    cheese: "provolone",
+    size: "large",
+    bread: "rye"
+});
+
+console.log( firstmeal instanceof sandwich);
+
+console.log( firstmeal );
+
